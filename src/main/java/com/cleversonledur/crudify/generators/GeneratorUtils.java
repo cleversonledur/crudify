4816a81f6b0d204a21f0bc7c4bf2e29826108848
cleversonledur <cleversonledur@gmail.com>
2018-12-27 14:59:32 -0200

    Starting service generator creation.

--- src/main/java/com/cleversonledur/crudify/generators/GeneratorUtils.java
+++ src/main/java/com/cleversonledur/crudify/generators/GeneratorUtils.java
@@ -3,10 +3,8 @@ package com.cleversonledur.crudify.generators;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.cleversonledur.crudify.generators.impl.ControllerGenerator;
 import com.cleversonledur.crudify.generators.impl.DtoGenerator;
 import com.cleversonledur.crudify.generators.impl.RepositoryGenerator;
-import com.cleversonledur.crudify.generators.impl.ServiceGenerator;
 
 public class GeneratorUtils {
 
@@ -14,8 +12,8 @@ public class GeneratorUtils {
         List<Generator> generators = new ArrayList<>();
         generators.add(new DtoGenerator());
         generators.add(new RepositoryGenerator());
-        generators.add(new ServiceGenerator());
-        generators.add(new ControllerGenerator());
+        //generators.add(new ServiceGenerator());
+        //generators.add(new ControllerGenerator());
         return generators;
     }
 }
