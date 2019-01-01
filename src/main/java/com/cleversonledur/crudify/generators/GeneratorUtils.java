ae7b98771811f8a3a3214c5286fcc2c4c70075fc
cleversonledur <cleversonledur@gmail.com>
2019-01-01 09:10:49 -0200

    Creating controller generator and refactoring

--- src/main/java/com/cleversonledur/crudify/generators/GeneratorUtils.java
+++ src/main/java/com/cleversonledur/crudify/generators/GeneratorUtils.java
@@ -3,6 +3,7 @@ package com.cleversonledur.crudify.generators;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.cleversonledur.crudify.generators.impl.ControllerGenerator;
 import com.cleversonledur.crudify.generators.impl.DtoGenerator;
 import com.cleversonledur.crudify.generators.impl.RepositoryGenerator;
 import com.cleversonledur.crudify.generators.impl.ServiceGenerator;
@@ -14,7 +15,7 @@ public class GeneratorUtils {
         generators.add(new DtoGenerator());
         generators.add(new RepositoryGenerator());
         generators.add(new ServiceGenerator());
-        //generators.add(new ControllerGenerator());
+        generators.add(new ControllerGenerator());
         return generators;
     }
 }
